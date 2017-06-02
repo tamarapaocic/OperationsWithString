9f0b7c850b198dfea49795a80f88b651f7d307bf
Tamara Paocic <tamara.paocic@hotmail.com>
2017-06-02 11:34:48 +0200

    dodane metode

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
-public class Main {
+public class Main  {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome! ");
+        System.out.println("Enter the string:");
+        
+        String userInput = input.nextLine();
+    
+        StringOperations operations = new StringOperations(userInput);
+        
+        operations.stringSize(userInput);
+        operations.charactersOnEvenPositions(userInput);
+        operations.charactersOnOddPositions(userInput);
+        operations.numberOfUppercaseChar(userInput);
+        operations.numberOfLowercaseChar(userInput);
+        operations.allCharactersWithoutLetters(userInput);
+        
+        input.close();
+	
 	}
-
 }
---
+++ StringOperations.java
@@ -0,0 +1,35 @@
+
+public class StringOperations {
+	
+	public StringOperations(String input){}
+	
+	public void stringSize(String input) {
+   		System.out.println("Size of string: " + input.length());
+   	}
+
+   	
+   	public void charactersOnEvenPositions(String userInput) {
+   	
+   	}
+
+   	public void charactersOnOddPositions(String userInput) {
+   		
+   	}
+
+   	
+   	public void numberOfUppercaseChar(String userInput) {
+   		
+   	}
+
+   	
+   	public void numberOfLowercaseChar(String userInput) {
+   		
+   	}
+
+   	public void allCharactersWithoutLetters(String userInput) {
+   		
+   	}
+
+
+	
+}
